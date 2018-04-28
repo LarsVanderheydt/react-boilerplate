103417cd48ebfdfdf115bba4c61e1e1eb63ecaa2
LarsVanderheydt <lars06@hotmail.be>
2018-04-28 20:10:42 +0200

    HashRouter instead of BrowserRouter  (avoiding deployment issues with react router)

--- src/js/containers/App.jsx
+++ src/js/containers/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { Route, HashRouter, Switch, Redirect } from 'react-router-dom';
 import { string } from 'prop-types';
 
 import DevTools from 'mobx-react-devtools';
@@ -10,25 +10,22 @@ import css from '../../css/style';
 import Home from './home';
 
 export default class App extends Component {
+  renderHome() {
+    return <Home />;
+  }
 
-    renderHome() {
-        return <Home />;
-    }
+  render() {
+    return (
+      <section>
+        {process.env.NODE_ENV !== `production` ? <DevTools /> : null}
 
-    render() {
-        return (
-            <section>
-
-                {process.env.NODE_ENV !== `production` ? <DevTools /> : null}
-
-                <Router>
-                    <Switch>
-                        <Route exact path='/home' render={this.renderHome} />
-                        <Route render={() => <Redirect to='/home' />} />
-                    </Switch>
-                </Router>
-
-            </section>
-        );
-    }
+        <HashRouter>
+          <Switch>
+            <Route exact path="/home" render={this.renderHome} />
+            <Route render={() => <Redirect to="/home" />} />
+          </Switch>
+        </HashRouter>
+      </section>
+    );
+  }
 }
